e4734aa23a93201fbf6ba9a80a02f665ee2f806e
amuliawijaya23 <amuliawijaya@gmail.com>
2023-01-16 13:36:06 -0800

    removes bootstrap and adds mui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 // react-router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import from react bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-
 // import custom components
 import Navigation from './components/Navigation';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navigation />
-    </>
+      <Routes>
+
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,16 +1,8 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+
 
 const Navigation = () => {
   return (
-    <Navbar bg='dark' variant='dark'>
-      <Container fluid>
-        <Navbar.Brand href='#home'>Covered Letter</Navbar.Brand>
-        <Nav className='me-auto'>
-          <Nav.Link href='#projects'>My Projects</Nav.Link>
-          <Nav.Link href='#new'>New</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+    <></>
   )
 };
 
