0e80cd9f761673a4d165270c86c49460fdbb6f2c
amuliawijaya23 <amuliawijaya@gmail.com>
2023-01-16 13:17:29 -0800

    adds placeholder navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+// react-router
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import from react bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+// import custom components
+import Navigation from './components/Navigation';
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navigation />
+    </>
   );
 }
 
